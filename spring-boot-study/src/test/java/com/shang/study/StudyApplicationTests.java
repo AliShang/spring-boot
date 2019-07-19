623a0e6a4b9886d678d5045f8ad71f03af3c0f09
Shanghc <albert_shc@163.com>
2019-07-19 14:18:30 +0800

    add Model spring-boot-study

--- spring-boot-study/src/test/java/com/shang/study/StudyApplicationTests.java
+++ spring-boot-study/src/test/java/com/shang/study/StudyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shang;
+package com.shang.study;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootBlogApplicationTests {
+public class StudyApplicationTests {
 
     @Test
     public void contextLoads() {
-        Boolean bool = true;
     }
 
 }
